dd0fe700ef22d316bbcb1cc398a64bda0b98b1d6
resurvenge <159584950+resurvenge@users.noreply.github.com>
2024-11-27 20:35:29 -0500

    Update mainfile.lua

--- mainfile.lua
+++ mainfile.lua
@@ -1,1 +1,9 @@
 
+local uilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/resurvenge/Qxi/master/guilibrary.lua", true))()
+
+
+
+local category = uilib.CreateCategory({ Name =  "Diego Hub", })
+local window = category.CreateWindow({ Name =  "Blatant"})
+
+local button = window.CreateButton({ Name = "cLICK ME", Function = function() print ("alr") end})
