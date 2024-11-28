0ab1a51ca8a4e630f03237e4c1a27cd7bddb19d4
resurvenge <159584950+resurvenge@users.noreply.github.com>
2024-11-28 16:52:33 -0500

    Update guilibrary.lua

--- guilibrary.lua
+++ guilibrary.lua
@@ -6,7 +6,7 @@ local UserInputService = game:GetService("UserInputService");
 
 
 if _G.loaded then
-	   _G.screenGui:Destroy()
+	   _G.shutdown()
 end
 
 local guilibrary = {
@@ -22,7 +22,14 @@ local guilibrary = {
 	Colors = {
 		WindowColor = Color3.fromRGB(142, 214, 255)
 	},
-	VERSION = "UPDT"
+	VERSION = "DATE: 11/28/2024, TIME: 5:00PM, PATCH NUMBER_1.1",
+	UISTROKES = true,
+	UISHADOWS = true,
+	THEMECOLORS = {
+		 BUTTON_COLOR = Color3.fromRGB(255, 105, 105),
+		 TEXT_COLOR = Color3.fromRGB(196, 196, 196),
+		 STROKES_COLOR = Color3.fromRGB(248, 109, 109)
+	}
 }
 
 local function loadQiex()
@@ -31,7 +38,7 @@ local function loadQiex()
 	local ScreenGui
 
 	local function scale(x, y)
-		return UDim2.new(x * guilibrary["Scale"]["Factor"], 0, y *guilibrary["Scale"]["Factor"], 0);
+		return UDim2.new(x * guilibrary["Scale"]["Factor"], 0, y * guilibrary["Scale"]["Factor"], 0);
 	end
 	local function udim2(x, y)
 		return UDim2.new(x, 0, y, 0);
@@ -51,7 +58,9 @@ local function loadQiex()
 		return instance({instance = "UICorner",Parent = Properties.parent, CornerRadius = UDim.new(Properties.scale)})
 	end
 	local function AddUIStroke(Properties)
-		return instance({instance = "UIStroke",Parent = Properties.parent, Color = Properties.color})
+		if guilibrary.UISTROKES then
+			 return instance({instance = "UIStroke",Parent = Properties.parent, Color = guilibrary["THEMECOLORS"]["STROKES_COLOR"]})
+		end
 	end
 
 	local function randomString()
@@ -85,6 +94,37 @@ local function loadQiex()
 		BackgroundColor3 = Color3.fromRGB(31, 31, 31);
 	})
 
+	local function guiShadow(parent_frame)
+		  if guilibrary.UISHADOWS then
+			local DropShadowHolder = Instance.new("Frame")
+			local DropShadow = Instance.new("ImageLabel")
+		
+		
+			DropShadowHolder.Name = "DropShadowHolder"
+			DropShadowHolder.Parent = parent_frame
+			DropShadowHolder.BackgroundTransparency = 1.000
+			DropShadowHolder.BorderSizePixel = 0
+			DropShadowHolder.Size = UDim2.new(1, 0, 1, 0)
+			DropShadowHolder.ZIndex = 0
+		
+			DropShadow.Name = "DropShadow"
+			DropShadow.Parent = DropShadowHolder
+			DropShadow.AnchorPoint = Vector2.new(0.5, 0.5)
+			DropShadow.BackgroundTransparency = 1.000
+			DropShadow.BorderSizePixel = 0
+			DropShadow.Position = UDim2.new(0.5, 0, 0.5, 0)
+			DropShadow.Size = UDim2.new(1, 47, 1, 47)
+			DropShadow.ZIndex = 0
+			DropShadow.Image = "rbxassetid://6015897843"
+			DropShadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
+			DropShadow.ImageTransparency = 0.500
+			DropShadow.ScaleType = Enum.ScaleType.Slice
+			DropShadow.SliceCenter = Rect.new(49, 49, 450, 450)
+		  end
+	end
+
+
+	guiShadow(MainFrame)
 	AddUICorner({parent = MainFrame, scale = guilibrary["Corners"]["Scaled"]})
 
 
@@ -112,7 +152,7 @@ local function loadQiex()
 		Name = "Title";
 		BackgroundTransparency = 1;
 		Font = Enum.Font.Gotham;
-		TextColor3 = Color3.fromRGB(227, 227, 227);
+		TextColor3 = guilibrary["THEMECOLORS"]["TEXT_COLOR"];
 		Text = guilibrary["title"];
 		Parent = TopBar;
 		Size = udim2(1,1);
@@ -157,47 +197,76 @@ local function loadQiex()
 	end
 
 	_G.shutdown = function()
-		MainFrame:Destroy()
+		_G.screenGui:Destroy()
 	end
 
 
+
+
 	function guilibrary.CreateCategory(options)
+		local CATEGORY_CONFIG = {
+			 ENALED = true
+		}
 		local Catergory = instance{
 			instance = "TextButton";
 			Parent = CatergoryContainer;
 			BackgroundTransparency = 0;
-			BackgroundColor3 = Color3.fromRGB(44, 44, 44);
+			BackgroundColor3 = guilibrary["THEMECOLORS"]["BUTTON_COLOR"],
 			Size = udim2(1, 0.13);
 			BorderSizePixel = 0;
 			Position = udim2(0, 0.010);
 			Text = "";
+			Name = options["Name"]
 		}
 		local CategoryTitle = instance{
 			instance = "TextLabel";
 			Name = "CategoryTitle";
 			BackgroundTransparency = 1;
 			Font = Enum.Font.Arial;
-			TextColor3 = Color3.fromRGB(227, 227, 227);
-			Text = options["Name"];
+			TextColor3 = guilibrary["THEMECOLORS"]["TEXT_COLOR"];
+			Text = "   " .. options["Name"];
 			Parent = Catergory;
 			Size = udim2(1,1);
 			TextSize = 15;
+			TextXAlignment = Enum.TextXAlignment.Left
 		}
 		CatergoryContainer.ChildAdded:Connect(function(child)
 			if child and child.Position then
-				child.Position+=udim2(0,0.13)
+				 child.Position+=udim2(0,0.13)
+				 print(child.Name)
 			end
 		end)
+
+		Catergory.InputBegan:Connect(function(input, gpe)
+			if gpe then return end;
+			if input.UserInputType == Enum.UserInputType.MouseButton1 then
+				 if CATEGORY_CONFIG.ENABLED then
+					tweenService:Create(Catergory, TweenInfo.new(1), {BackgroundColor3 = MainFrame.BackgroundColor3}):Play()
+					tweenService:Create(CategoryTitle, TweenInfo.new(1), {TextColor3 = guilibrary["THEMECOLORS"]["TEXT_COLOR"]}):Play()
+					 CATEGORY_CONFIG.ENABLED = false
+				 else
+					 tweenService:Create(Catergory, TweenInfo.new(1), {BackgroundColor3 = guilibrary["THEMECOLORS"]["BUTTON_COLOR"]}):Play()
+					 tweenService:Create(CategoryTitle, TweenInfo.new(1), {TextColor3 = Color3.fromRGB()}):Play()
+					 CATEGORY_CONFIG.ENABLED = true
+				 end
+			end
+		end)
+		
 		return {
 			CreateWindow = function(optionstable)
+				local WINDOW_CONFIG = {
+					VISIBLE = false
+				}
 				local window = instance{
 					instance = "Frame";
-					Parent = MainFrame,
+					Parent = ScreenGui,
 					BackgroundTransparency = 0.010,
 					BackgroundColor3 = Color3.fromRGB(31, 31, 31),
-					Size = scale(1,0.95),
+					Size = scale(0.11, 0.455);
 					Position = udim2(0.20, 0.100)
 				}
+
+				guiShadow(window)
 				AddUICorner({parent = window, scale = guilibrary["Corners"]["Scaled"]})
 				AddUIStroke({parent = window, color = guilibrary["Colors"]["WindowColor"]})
 				local WindowTitle = instance{
@@ -212,42 +281,62 @@ local function loadQiex()
 				local WindowTitleText = instance{
 					instance = "TextLabel",
 					Parent = WindowTitle,
-					Text = "     ".. optionstable.Name,
-					TextSize = 14;
-					TextColor3 = Color3.fromRGB(215, 215, 215),
+					Text = " " .. optionstable.Name,
+					TextSize = 20,
+					TextColor3 = guilibrary["THEMECOLORS"]["TEXT_COLOR"],
 					Font = Enum.Font.Arial;
 					BackgroundTransparency = 1,
 					BorderSizePixel = 0,
-					Size = udim2(1,1),
-					TextXAlignment = Enum.TextXAlignment.Left
+					TextXAlignment = Enum.TextXAlignment.Left,
+					Position = udim2(0, 0.12),
+					TextScaled = true,
+					AutomaticSize = Enum.AutomaticSize.X,
+					Size = udim2(0.5, 0.5)
+
 				}
 				local WindowButtonContainer = instance{
 					instance = "Frame",
 					Parent = window,
-					Size = scale(1, 0.30),
-					Position = udim2(0,0.050),
-					BackgroundTransparency = 1
-				}
+					Size = udim2(1, 0.1),
+					Position = udim2(0,0.1),
+					BackgroundTransparency = 1,
 
-				WindowButtonContainer.ChildAdded:Connect(function(child)
-					if child and child.Position then
-						 child.Position+=udim2(0,0.13)
-					end
-				end)
+				}
 
 				dragUI(window)
 
+				Catergory.InputBegan:Connect(function(input)
+					    if input.UserInputType==Enum.UserInputType.MouseButton1 then
+						        if WINDOW_CONFIG.VISIBLE then
+									 window.Visible = false
+									 WINDOW_CONFIG.VISIBLE = false
+								else
+									window.Visible = true
+									 WINDOW_CONFIG.VISIBLE = true
+								end
+						end
+				end)
+				ScreenGui.ChildAdded:Connect(function(child)
+					if child and child.Position then
+						  child.Position += udim2(0.12)
+					end
+			     end)
+
 				return {
 					CreateButton = function(buttonsoptions)
+						local BUTTON_CONFIG = {
+							 ENABLED = true
+						}
 						local button = instance{
 							instance = "TextButton",
 							BackgroundTransparency = 0;
-							BackgroundColor3 = window.BackgroundColor3,
+							BackgroundColor3 =  window.BackgroundColor3,
 							Text = "",
-							Size = udim2(1,0.45),
 							Parent = WindowButtonContainer,
 							BorderSizePixel = 0,
-							Position = udim2(0, 0.20)
+							Position = udim2(0, 0),
+							Name = buttonsoptions["Name"],
+							Size = udim2(1,1)
 						}
 						local ButtonTitle = instance{
 							instance = "TextLabel",
@@ -256,29 +345,42 @@ local function loadQiex()
 							Size = udim2(1,1),
 							TextXAlignment = Enum.TextXAlignment.Left,
 							Font = Enum.Font.Arial,
-							TextColor3 = Color3.fromRGB(227, 228, 230),
+							TextColor3 = guilibrary["THEMECOLORS"]["TEXT_COLOR"],
 							TextSize = 13,
-							Text ="   " ..  buttonsoptions.Name or "Label"
+							Text = "   " ..  buttonsoptions.Name or "Label"
 						}
+						button.InputBegan:Connect(function(input, gpe)
+							if gpe then return end;
+							if input.UserInputType == Enum.UserInputType.MouseButton1 then
+								 if BUTTON_CONFIG.ENABLED then
+									 tweenService:Create(button, TweenInfo.new(1), {BackgroundColor3 = guilibrary["THEMECOLORS"]["BUTTON_COLOR"]}):Play()
+									 tweenService:Create(ButtonTitle, TweenInfo.new(1), {TextColor3 = Color3.fromRGB()}):Play()
+									 BUTTON_CONFIG.ENABLED = false
+								 else
+									tweenService:Create(button, TweenInfo.new(1), {BackgroundColor3 = window.BackgroundColor3}):Play()
+									tweenService:Create(ButtonTitle, TweenInfo.new(1), {TextColor3 = guilibrary["THEMECOLORS"]["TEXT_COLOR"]}):Play()
+									BUTTON_CONFIG.ENABLED = true
+								 end
+							end
+						end)
                         if buttonsoptions.Function then
                             button.InputBegan:Connect(function(input, gpe)
                                 if gpe then return end;
                                 if input.UserInputType == Enum.UserInputType.MouseButton1 then
-                                     buttonsoptions.Function()
+                                      if BUTTON_CONFIG.ENABLED then
+										   buttonsoptions.Function(BUTTON_CONFIG.ENABLED)
+									  end
                                 end
                             end)
+							WindowButtonContainer.ChildAdded:Connect(function(child)
+								if child and child.Position then
+									 child.Position+=udim2(0,1)
+									 print(child.Name)
+								end
+							end)
                         end
 					end
 				}
-			end,
-
-			OnClick = function(func)
-				Catergory.InputBegan:Connect(function(input, gpe)
-					if gpe then return end;
-					if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-						func()
-					end
-				end)
 			end
 		}
 	end
