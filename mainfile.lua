128986991698f248568f3455062bac96c803c25c
resurvenge <159584950+resurvenge@users.noreply.github.com>
2024-11-28 23:20:37 -0500

    Update mainfile.lua

--- mainfile.lua
+++ mainfile.lua
@@ -1,9 +1,29 @@
+local Debris = game:GetService("Debris")
 
 local uilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/resurvenge/Qxi/master/guilibrary.lua", true))()
+if uilib then
+	print("ok")
+	print(uilib.title)
+	print(uilib.VERSION)
+end
 
+local BlatantCategory = uilib.CreateCategory({ Name =  "Blatant", })
+local WorldCategory = uilib.CreateCategory({ Name =  "World", })
 
+local Blatant = BlatantCategory.CreateWindow({ Name =  "Blatant"})
+local World = WorldCategory.CreateWindow({ Name =  "World", })
 
-local category = uilib.CreateCategory({ Name =  "Diego Hub", })
-local window = category.CreateWindow({ Name =  "Blatant"})
+Blatant.CreateButton({ Name = "jumpHeight", Function = function()
+	   game.Players.LocalPlayer.Character.Humanoid.JumpHeight = 30
+end})
 
-local button = window.CreateButton({ Name = "cLICK ME", Function = function() print ("alr") end})
+Blatant.CreateButton({ Name = "Speed {dont try in bedwars]", Function = function()
+	  local old = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
+	  game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+end})
+
+World.CreateButton({ Name = "ESP", Function = function(toggle)
+	    if toggle then
+			   print("sucessful")
+		end
+end})
