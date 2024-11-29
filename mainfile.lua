f2d0badb9a47ab1bdfdfac6a425177fc2f2a09a0
resurvenge <159584950+resurvenge@users.noreply.github.com>
2024-11-28 23:24:10 -0500

    Update mainfile.lua

--- mainfile.lua
+++ mainfile.lua
@@ -1,11 +1,14 @@
 local Debris = game:GetService("Debris")
-
+local playerService = game:GetService("Players")
 local uilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/resurvenge/Qxi/master/guilibrary.lua", true))()
 if uilib then
 	print("ok")
 	print(uilib.title)
 	print(uilib.VERSION)
 end
+
+
+print("script ver. 1")
 
 local BlatantCategory = uilib.CreateCategory({ Name =  "Blatant", })
 local WorldCategory = uilib.CreateCategory({ Name =  "World", })
@@ -24,6 +27,25 @@ end})
 
 World.CreateButton({ Name = "ESP", Function = function(toggle)
 	    if toggle then
-			   print("sucessful")
+			   for _, player in pairs(playerService:GetChildren()) do
+				       if player then
+						      if player.Character then
+								     local HighlightInstance = Instance.new("Highlight")
+									 HighlightInstance.Parent = player.Character
+							  end
+					   end
+			   end
+			else
+				for _, player in pairs(playerService:GetChildren()) do
+					if player then
+						   if player.Character then
+								  for _, obj in pairs(player.Character:GetChildren()) do
+									     if obj:IsA("Highlight") then
+											    obj:Destroy()
+										 end
+								  end
+						   end
+					end
+			end
 		end
 end})
